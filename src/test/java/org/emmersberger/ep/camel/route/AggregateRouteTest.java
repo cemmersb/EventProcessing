bc90ed5e29e406a8bbef7d9362617a3ec5faa5cb
Christoph Emmersberger <cemmersb@googlemail.com>
2013-03-01 11:04:04 +0100

    + adding producer template + initializing test data

--- src/test/java/org/emmersberger/ep/camel/route/AggregateRouteTest.java
+++ src/test/java/org/emmersberger/ep/camel/route/AggregateRouteTest.java
@@ -1,5 +1,11 @@
 package org.emmersberger.ep.camel.route;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.emmersberger.ep.camel.common.AggregateProduct;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,8 +19,23 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration
 public class AggregateRouteTest {
   
+  @Produce(uri = AggregateRoute.DIRECT_START_AGGREGATE)
+  private ProducerTemplate aggregateTemplate;
+  
+  private final List<AggregateProduct> aggregateProducts = new ArrayList<AggregateProduct>();
+  
+  @Before
+  public void initializeEnrichCustomer() {
+    aggregateProducts.clear();
+    aggregateProducts.add(new AggregateProduct("Small Parcel", "Max Size: 60x30x15, Max Weight: 2kg", 3.90));
+    aggregateProducts.add(new AggregateProduct("Parcel", "Max Size: 120x60x60, Max Weight: 10kg", 4.90));
+    aggregateProducts.add(new AggregateProduct("Large Parcel", "Max Size: 4m^3, Max Weight: 100kg", 44.90));
+  }
+  
   @Test
   public void testAggregateRoute() {
-  
+    for (AggregateProduct aggregateProduct : aggregateProducts) {
+      aggregateTemplate.sendBody(aggregateProduct);
+    }
   }
 }
